e165393ec006a9ee0e5ee33376e63e9297fab532
shimaenaga <admin@shimaenaga.dev>
2025-06-16 21:37:32 +0900

    한국 Locale 설정 및 날짜 선택기 수정

--- src/app/components/database/database.component.ts
+++ src/app/components/database/database.component.ts
@@ -4,7 +4,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatPaginatorModule, PageEvent, MatPaginatorIntl } from '@angular/material/paginator';
 import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatBadgeModule } from '@angular/material/badge';
@@ -21,7 +21,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatTimepickerModule } from '@angular/material/timepicker';
-import { MatNativeDateModule } from '@angular/material/core';
+import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 import {
@@ -53,6 +53,29 @@ import {
     MatTimepickerModule,
     MatNativeDateModule,
     ReactiveFormsModule,
+  ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'ko-KR' },
+    {
+      provide: MatPaginatorIntl,
+      useFactory: () => {
+        const intl = new MatPaginatorIntl();
+        intl.itemsPerPageLabel = '';
+        intl.nextPageLabel = '';
+        intl.previousPageLabel = '';
+        intl.firstPageLabel = '';
+        intl.lastPageLabel = '';
+        intl.getRangeLabel = (page: number, pageSize: number, length: number) => {
+          if (length === 0 || pageSize === 0) {
+            return `0 / ${length}`;
+          }
+          const startIndex = page * pageSize;
+          const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
+          return `${startIndex + 1} - ${endIndex} / ${length}`;
+        };
+        return intl;
+      }
+    }
   ],
   templateUrl: './database.component.html',
 })
@@ -420,6 +443,9 @@ export class DatabaseComponent implements OnInit {
     MatTimepickerModule,
     MatNativeDateModule,
     ReactiveFormsModule,
+  ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'ko-KR' }
   ],
   templateUrl: './database-dialog.component.html',
 })
